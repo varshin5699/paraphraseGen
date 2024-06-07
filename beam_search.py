a0b9925d220b059edd21c730f207bbcde35ce0aa
varshin5699 <136289580+varshin5699@users.noreply.github.com>
2024-06-07 21:27:17 +0530

    Update beam_search.py

--- beam_search.py
+++ beam_search.py
@@ -80,7 +80,7 @@ class Beam(object):
 
         # bestScoresId is flattened beam x word array, so calculate which
         # word and beam each score came from
-        prev_k = bestScoresId / num_words
+        prev_k = bestScoresId // num_words
         self.prevKs.append(prev_k)
         self.nextYs.append(bestScoresId - prev_k * num_words)
 
